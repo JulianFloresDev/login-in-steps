e84edeb864e508630b4d16c0a736f29b4f149442
Julian Flores <julianfloresdev@gmail.com>
2023-08-06 23:05:09 -0300

    fix: type when write sql on config

--- src/db.mjs
+++ src/db.mjs
@@ -16,16 +16,16 @@ const connection = createConnection({
 
 connection.query('USE testing_ali_fullstack', (error, results) => {
   if (error) throw new Error(error);
-  const query = `CREATE TABLE IF NOT EXISTS 'users_test_julianflores' (
-    'id' INT NOT NULL AUTO_INCREMENT,
-    'nombre' VARCHAR(255) NOT NULL,
-    'segundo_nombre' VARCHAR(255),
-    'apellido_paterno' VARCHAR(255) NOT NULL,
-    'apellido_materno' VARCHAR(255),
-    'fecha_de_nacimiento' DATE,
-    'email' VARCHAR(255) NOT NULL,
-    'telefono' VARCHAR(20),
-    PRIMARY KEY ('id')
+  const query = `CREATE TABLE IF NOT EXISTS \`users_test_julianflores\` (
+    \`id\` INTEGER NOT NULL AUTO_INCREMENT,
+    \`nombre\` VARCHAR(255) NOT NULL,
+    \`segundo_nombre\` VARCHAR(255),
+    \`apellido_paterno\` VARCHAR(255) NOT NULL,
+    \`apellido_materno\` VARCHAR(255),
+    \`fecha_de_nacimiento\` DATE,
+    \`email\` VARCHAR(255) NOT NULL,
+    \`telefono\` VARCHAR(20),
+    PRIMARY KEY (\`id\`)
 )`;
 
   connection.query(query, (error, results) => {
