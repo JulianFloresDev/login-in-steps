7ee9134b4fd038ecd796faf4146db34d0fe19125
Julian Flores <julianfloresdev@gmail.com>
2023-08-06 22:27:17 -0300

    fix: cors config

--- src/app.mjs
+++ src/app.mjs
@@ -6,9 +6,12 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors({
-  origin: []
-}));
+app.use(
+  cors({
+    origin: '*',
+    methods: ['GET', 'POST'],
+  })
+);
 
 app.use(express.urlencoded({ extended: false }));
 
